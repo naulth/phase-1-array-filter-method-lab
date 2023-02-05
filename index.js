fae3c01be6781eda79a072d6a9e1b4a821bd7bbb
Hayden Nault <naulth@gmail.com>
2023-02-05 17:34:56 -0500

    submitting lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution here
+function findMatching(drivers, string){
+    const result = drivers.filter(checkDrivers)
+    return result
+
+    function checkDrivers(driver){
+        return driver.toLowerCase() === string.toLowerCase()
+    }
+}
+
+
+
+function fuzzyMatch(drivers, string){
+    const fuzzyResult = drivers.filter(driver => driver.toLowerCase().indexOf(string.toLowerCase()) === 0)
+    return fuzzyResult
+}
+
+
+
+function matchName(drivers, string){
+    const matchedName = drivers.filter(driver => driver.name === string)
+    console.log(matchedName)
+    return matchedName
+}
